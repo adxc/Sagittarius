bcc6960363affc14cfe3fb9baf9040a68ab5bd2b
Andy Xic <andyxic@163.com>
2018-07-19 15:03:49 +0800

    upd:分页

--- src/app/sgr-workspace/sgr-workspace-folder/sgr-workspace-folder.component.ts
+++ src/app/sgr-workspace/sgr-workspace-folder/sgr-workspace-folder.component.ts
@@ -1,29 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
-import { ManagerService } from '../../sgr-service/manager.service';
 @Component({
   selector: 'app-sgr-workspace-folder',
   templateUrl: './sgr-workspace-folder.component.html',
   styleUrls: ['./sgr-workspace-folder.component.scss'],
 })
 export class SgrWorkspaceFolderComponent implements OnInit {
-  private tabs: string[] = [];
-  private index = 0;
-  constructor(private managerService: ManagerService) { }
+  constructor() { }
   ngOnInit() {
-    this.folderDetial();
-  }
-  closeTab (tab: string) {
-    this.tabs.splice(this.tabs.indexOf(tab), 1);
-  }
-  folderDetial () {
-    this.managerService.managerItem.subscribe(folder => {
-      if (this.tabs.indexOf(folder.name) === -1) {
-        this.tabs.push(folder.name);
-        this.index = this.tabs.length  - 1;
-      } else {
-        this.index = this.tabs.indexOf(folder.name);
-      }
-    });
   }
 }
